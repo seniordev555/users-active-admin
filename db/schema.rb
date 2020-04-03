7697cd03911af850920a75a033943f1d2117c584
seniordev555 <17595822+seniordev555@users.noreply.github.com>
2020-04-04 02:10:39 +0800

    add genre

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_005313) do
+ActiveRecord::Schema.define(version: 2020_04_03_175423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,12 @@ ActiveRecord::Schema.define(version: 2020_04_03_005313) do
     t.string "last_name"
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', first_name: 'Admin', last_name: 'Example') if Rails.env.development?
+if Rails.env.development?
+  if (!AdminUser.exists?)
+    AdminUser.create!(
+      email: 'admin@example.com',
+      password: 'password',
+      password_confirmation: 'password',
+      first_name: 'Admin',
+      last_name: 'Example'
+    )
+  end
+
+  if (!Genre.exists?)
+    5.times do
+      Genre.create!(name: Faker::Book.unique.genre)
+    end
+  end
+end
