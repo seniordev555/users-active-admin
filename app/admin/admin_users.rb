2c38bff59316890a8962edbeaff04423300a9bcf
seniordev555 <17595822+seniordev555@users.noreply.github.com>
2020-04-03 09:18:19 +0800

    add avatar to users

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -1,9 +1,20 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :password, :password_confirmation,
+                :first_name, :last_name, :avatar
 
   index do
     selectable_column
     id_column
+    column 'Avatar' do |admin_user|
+      if admin_user.avatar.attached?
+        image_tag url_for(admin_user.avatar), width: 48, height: 48
+      else
+        image_tag image_path('profile.jpg'), width: 48, height: 48
+      end
+    end
+    column 'Name' do |admin_user|
+      link_to "#{admin_user.first_name} #{admin_user.last_name}", admin_admin_user_path(admin_user)
+    end
     column :email
     column :current_sign_in_at
     column :sign_in_count
@@ -18,11 +29,31 @@ ActiveAdmin.register AdminUser do
 
   form do |f|
     f.inputs do
+      f.input :first_name
+      f.input :last_name
       f.input :email
       f.input :password
       f.input :password_confirmation
+      f.input :avatar, as: :file
     end
     f.actions
+  end
+
+  show do
+    attributes_table do
+      row :avatar do |admin_user|
+        if admin_user.avatar.attached?
+          image_tag url_for(admin_user.avatar), width: 48, height: 48
+        else
+          image_tag image_path('profile.jpg'), width: 48, height: 48
+        end
+      end
+      row :first_name
+      row :last_name
+      row :email
+      row :created_at
+      row :updated_at
+    end
   end
 
 end
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,10 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :avatar
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
